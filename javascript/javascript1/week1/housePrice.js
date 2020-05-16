363e73bf8a5491496bd7cf8c3e207b2e2772796f
Maher <maherhussain6@gmail.com>
2020-05-16 23:32:18 +0000

    hous price modified

--- javascript/javascript1/week1/housePrice.js
+++ javascript/javascript1/week1/housePrice.js
@@ -1,44 +1,38 @@
 //housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300
 
-// perters parameters 
-var wide = 8 ;
-var deep = 10;
-var height = 10;
-var gardinSize = 100;
+// perters parameters
+let wide = 8;
+let deep = 10;
+let height = 10;
+let gardinSize = 100;
 
 const perterVolumeInMeters = wide * deep * height;
-const peterHousPrice = perterVolumeInMeters *2.5 * 1000 + gardinSize * 300 ;
+const peterHousPrice = perterVolumeInMeters * 2.5 * 1000 + gardinSize * 300;
 
 const peterPayment = 2500000;
 //console.log(peterPayment - peterHousPrice);
-if (peterHousPrice < peterPayment){
-    console.log("Peter paied too much "  );
-    }
-    else{
-        console.log("Peter paied too little.");
-    
-    }
-
+if (peterHousPrice < peterPayment) {
+  console.log("Peter paied too much ");
+} else {
+  console.log("Peter paied too little.");
+}
 
 // julia parameters
 
-var wide = 5;
-var deep = 11;
-var height = 8;
-var gardinSize = 70;
+wide = 5;
+deep = 11;
+height = 8;
+gardinSize = 70;
 
-const juliaVolumeInMeters = wide*deep*height;
-const juliaHousPrice = perterVolumeInMeters *2.5 * 1000 + gardinSize * 300 ;
+const juliaVolumeInMeters = wide * deep * height;
+const juliaHousPrice = perterVolumeInMeters * 2.5 * 1000 + gardinSize * 300;
 
 //console.log("julia " + juliaHousPrice);
 
 const juliaPayment = 1000000;
 
-if (juliaPayment < juliaHousPrice)
-{
-    console.log("Julia paied  too little");
-    }
-    else{
-        console.log("Julia paied too much.");
-    
-    }
+if (juliaPayment < juliaHousPrice) {
+  console.log("Julia paied  too little");
+} else {
+  console.log("Julia paied too much.");
+}
