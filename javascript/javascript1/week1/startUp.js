607d9355402f29b9cbae3321348a025158b1d8bd
Maher <maherhussain6@gmail.com>
2020-05-16 23:39:03 +0000

    startup file modified

--- javascript/javascript1/week1/startUp.js
+++ javascript/javascript1/week1/startUp.js
@@ -1,6 +1,34 @@
-var firstWord = ["Maher", "Ahmad" ,"jan" , "Essam" , "Florin","Hello ", "World","startup","javascript","html"];
-var secondWord =["Hussain", "css", "bootstrap", "jquery","c#" ,"asp.net","php","react","vue","vuc"];
-7
-const randomNumber = Math.floor(Math.random() * 10) + 0
+var firstWord = [
+  "Maher",
+  "Ahmad",
+  "jan",
+  "Essam",
+  "Florin",
+  "Hello ",
+  "World",
+  "startup",
+  "javascript",
+  "html",
+];
+var secondWord = [
+  "Hussain",
+  "css",
+  "bootstrap",
+  "jquery",
+  "c#",
+  "asp.net",
+  "php",
+  "react",
+  "vue",
+  "vuc",
+];
+
+const randomNumber = Math.floor(Math.random() * 10) + 0;
 var startUpName = firstWord[randomNumber] + " " + secondWord[randomNumber];
-console.log("the startup is " + startUpName + " and it conatins " + startUpName.length +" characters");
+console.log(
+  "the startup is " +
+    startUpName +
+    " and it conatins " +
+    startUpName.length +
+    " characters"
+);
