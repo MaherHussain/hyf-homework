bc8895d9b95b93f9bc315f5018c1d2f9619dea7f
Maher <maherhussain6@gmail.com>
2020-05-18 03:11:03 +0000

    full name file added

---
+++ javascript/javascript1/week2/fullNameEx.js
@@ -0,0 +1,20 @@
+
+/*let firstName1 = "Maher";
+let sureName1 = "Hussain";
+let firstName2 = "Essam";
+let sureName2 = "Ahmad";*/
+function getFullName (firstName , sureName ){
+  
+console.log( firstName, sureName );
+
+return firstName + sureName;
+
+}
+
+ 
+  const fullName1= getFullName("Maher", "Hussain");
+  const fullName2 =getFullName("Essam", "Ahmad");
+  
+
+
+  
