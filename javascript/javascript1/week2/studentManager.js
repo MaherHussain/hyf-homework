83161b501842738174f9939de89ca91c9705a0d8
Maher <maherhussain6@gmail.com>
2020-05-24 01:59:40 +0000

    student name is modified

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -5,22 +5,25 @@ const class07Students = [];
 function addStudentToClass(studentName) {
   let result;
   // You write code here
-  if (studentName == "Queen") {
-    class07Students.unshift(studentName);
-    result = "welcome Queen Margrethe you have place between us ";
+  if (studentName === "Queen") {
+    if (class07Students.includes("Queen")) {
+      result = "sorry we have one Queen you cant add more";
+    } else {
+      class07Students.push(studentName);
+      result = "welcome Queen Margrethe you have place between us";
+    }
   } else if (class07Students.length >= 6) {
-    return fales;
     result = "Cannot add more students to class 07";
   } else if (class07Students.includes(studentName)) {
     result = "Student " + studentName + " is already in the class";
-  } else if (studentName == " ") {
+  } else if (studentName.trim === "") {
     result = "write syudent name please";
   } else {
     class07Students.push(studentName);
     result = "student is added";
   }
 
-  return alert(result);
+  return console.log(result);
 }
 
 function getNumberOfStudents() {
