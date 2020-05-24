b92544fef671eb78f5cf43037022051d8a1a1ff4
Maher <maherhussain6@gmail.com>
2020-05-24 01:35:51 +0000

    let vaiable removed

--- javascript/javascript1/week2/weatherWear.js
+++ javascript/javascript1/week2/weatherWear.js
@@ -1,18 +1,13 @@
 function weather(temperature) {
-  let output;
   if (temperature < 5) {
-    output =
-      "it's winter  and it's very cold,  you have to wear coat and hat and cloves ";
+    return "it's winter  and it's very cold,  you have to wear coat and hat and cloves ";
   } else if (5 < temperature && temperature < 15) {
-    output =
-      "it's  still winter but it's not so much cold, so it's good just jacket to wear  ";
+    return "it's  still winter but it's not so much cold, so it's good just jacket to wear  ";
   } else if (15 < temperature && temperature < 25) {
-    output = "it's  sommer and  it's hot , so  Tshirt is enough ......  ";
+    return "it's  sommer and  it's hot , so  Tshirt is enough ......  ";
   } else {
-    output =
-      "it's  sommer and it's very hot , so  go to beach and don't wear clothes ......  ";
+    return "it's  sommer and it's very hot , so  go to beach and don't wear clothes ......  ";
   }
-  return output;
 }
-const userwear = weather(24);
+const userwear = weather(30);
 console.log(userwear);
