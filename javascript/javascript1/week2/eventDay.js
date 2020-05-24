1da954af6f47293ac25e67d38d3ef334049636a9
Maher <maherhussain6@gmail.com>
2020-05-24 00:21:47 +0000

    some changes done

--- javascript/javascript1/week2/eventDay.js
+++ javascript/javascript1/week2/eventDay.js
@@ -1,22 +1,26 @@
-  let weekdays =["sunday", "monday","tuesday","Wednesday","Thursday","Friday","Saturday"];
- 
-    let  today  = new Date().getDay(); 
-    let eventDay ;
-    console.log(today);
+let weekdays = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let today = new Date().getDay();
+let eventDay;
+console.log(today);
 
 function getEventWeekday(eventDayNum) {
+  eventDayNum = eventDayNum + today - 1;
+  console.log(eventDayNum);
 
-eventDayNum = eventDayNum + today - 1
-console.log(eventDayNum)
-
-    if(eventDayNum < 7){
-        return weekdays[eventDayNum ]
-    }
-    else{
-        return weekdays[eventDayNum % 7 ]
-    }
-
-     
+  if (eventDayNum < 7) {
+    return weekdays[eventDayNum];
+  } else {
+    return weekdays[eventDayNum % 7];
+  }
 }
 eventDay = getEventWeekday(7);
-console.log( " event day will be " + eventDay);
+console.log(" event day will be " + eventDay);
