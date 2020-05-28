19f49654b98c79ff558b862156a57d4b897f0026
Maher <maherhussain6@gmail.com>
2020-05-28 00:58:21 +0000

    eventDay modefied

--- javascript/javascript1/week2/eventDay.js
+++ javascript/javascript1/week2/eventDay.js
@@ -13,14 +13,28 @@ let eventDay;
 console.log(weekdays[today]);
 
 function getEventWeekday(day) {
-  day = today - 1 + day;
-  console.log(weekdays[day]);
+  //day = today - 1 + day;
 
-  if (day < 7) {
+  /*if (day < 7) {
     return weekdays[day];
   } else {
     return weekdays[day % 7];
-  }
+  }*/
+  return weekdays[(day + today) % 7];
 }
 eventDay = getEventWeekday(5);
 console.log(" event day will be " + eventDay);
+/*function getEventWeekday(day) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date().getDay();
+  return days[(day + today) % 7];
+}
+*/
