c2330b3eb6ce736db7e276c31ac810a45e171c4b
Maher <maherhussain6@gmail.com>
2020-05-29 03:21:16 +0000

    getNot function added

--- javascript/javascript1/week3/notePad.js
+++ javascript/javascript1/week3/notePad.js
@@ -12,3 +12,21 @@ saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
 console.log(notes);
+
+function getNote(id) {
+  for (i = 0; i < notes.length; i++) {
+    if (id === notes[i].id) {
+      return notes[i];
+    }
+  }
+  if (isNaN(id)) {
+    console.log("the input is not a number ");
+  } else {
+    console.log("there's no note with this id ");
+  }
+
+  // your code here
+}
+
+const firstNote = getNote(3);
+console.log(firstNote);
