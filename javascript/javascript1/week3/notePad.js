1c66b4331a698b51acc1e2b4740737e8948e974d
Maher <maherhussain6@gmail.com>
2020-06-01 23:04:27 +0000

    note pad modefied

--- javascript/javascript1/week3/notePad.js
+++ javascript/javascript1/week3/notePad.js
@@ -1,33 +1,30 @@
 const notes = []; // collection of objects
 function saveNote(content, id) {
-  let elem = {}; // first we made an object var
-  elem.id = id;
-  elem.note = content;
-  notes.push(elem); // we pushed the object into arr
-
-  // write some code here
+  notes.push({ content, id }); // we pushed the object into arr
 }
 
 saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
-console.log(notes);
-
 // here go getNot function
 
+/**
+ * @param {number} id
+ */
 function getNote(id) {
-  for (i = 0; i < notes.length; i++) {
-    if (id === notes[i].id) {
+  if (typeof id !== "number") {
+    console.error("the input is not a number ");
+    return;
+  }
+
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === id) {
       return notes[i];
     }
   }
-  if (isNaN(id)) {
-    console.log("the input is not a number ");
-  } else {
-    console.log("there's no note with this id ");
-  }
 
-  // your code here
+  throw new Error("note not found!");
+  //console.error(" note not found !");
 }
 
 const firstNote = getNote(3);
@@ -35,16 +32,15 @@ console.log(firstNote);
 
 // here go all notes formatted function
 function logOutNotesFormatted() {
-  for (i = 0; i < notes.length; i++) {
+  for (let i = 0; i < notes.length; i++) {
     console.log(
       "The note with id: " +
-        notes[i]["id"] +
+        notes[i].id +
         ", has the following note text: " +
         '"' +
-        notes[i]["note"] +
+        notes[i].content +
         '"'
     );
   }
-  // your code here
 }
 logOutNotesFormatted();
