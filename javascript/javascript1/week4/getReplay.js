54d55cdf83e5f99df526c87e85c1a424cbcc0a45
Maher <maherhussain6@gmail.com>
2020-06-11 10:13:59 +0000

    get replay exercis modefied

--- javascript/javascript1/week4/getReplay.js
+++ javascript/javascript1/week4/getReplay.js
@@ -1,39 +1,41 @@
-let toDo = [];
+let toDoList = [];
+
+
 
 function getReply(command) {
-    const commandWords = command.split(' ');
+    let name;
+    const commandWords = command.split(" ");
   if (command.startsWith("hello my name ")) {
-    const name = commandWords[commandWords.length - 1];
+   name = commandWords[commandWords.length - 1];
     return " Nice to meet you " + name;
   } else if (command.startsWith("What is my name")) {
-    return " Your name is Maher";
-  } else if (command.startsWith("Add")) {
-    const activity = commandWords[1];
+    name = "Ahmad";
+    return " Your name is "+ name ;
+  } else if (command.startsWith("Add") && command.endsWith("to my todo")) {
+    const activity = command.replace("Add", "").replace("to my todo","");
     
-    if(toDo.includes(activity))
-    {
-      return activity  + " allready exist"
-    }
-    else{
-        toDo.push(activity);
-        return activity + " added to your todo";
+    if (toDoList.includes(activity)) {
+      return activity + " allready exist";
+    } else {
+      toDoList.push(activity);
+      return activity + "added to your todo";
     }
     
   }
-   else if (command.startsWith("Remove")) {
-    const activity = commandWords[1];
-    const remo = toDo.indexOf(activity);
-    if(remo > -1 ){
-      toDo.splice(remo,1);
+   else if (command.startsWith("Remove") && command.endsWith("from my todo")) {
+    const activity = command.replace("Remove", "").replace("from my todo","");
+    const todo = toDoList.indexOf(activity);
+    if (todo > -1) {
+      toDoList.splice(todo, 1);
     }
     return activity + " removed from your todo";
   }
   else if(command.startsWith("What is on my todo?"))
   {
-      if (toDo.length == 0) {
+      if (toDoList.length == 0) {
         return "there is no thing to do ";
       } else {
-        return "you have " + toDo.length + " todos " + '"' + toDo + '"';
+        return "you have " + toDoList.length + " todos " + '"' + toDoList + '"';
       }
   }
   else if(command === 'what day is it today?')
@@ -46,20 +48,36 @@ function getReply(command) {
   }
 
   else if (command.startsWith ("what is")) {
-    const commandWords = command.split(' ');
-    console.log(commandWords);
-    let num1 = commandWords[commandWords.length - 3] ;
-    let num2 = commandWords[commandWords.length - 1];
-         return parseInt(num1) + parseInt(num2);
+    
+    
+    let num1 =parseInt(commandWords[2])  ;
+    let num2 = parseInt(commandWords[4]);
+    let  operation = commandWords[3];
+    if(num1 && num2){
+      switch (operation) {
+        
+        case "+":
+          return num1 + num2;
+        case "-":
+          return num1 - num2;
+        case "*":
+          return num1 * num2;
+        case "/":
+          return num1 / num2;
+          default:
+            return 'there is no operation';
+      }
+    }
+         
        }
       
        else if( command.startsWith('set a timer for ')){
           const commandWords = command.split(" ");
-          let time = parseInt(commandWords[commandWords.length - 2]);
+          let seconds = parseInt(commandWords[4]);
           setTimeout(function () {
             console.log("timer is done");
-          }, time);
-          return "timer set for " + time  +" min";
+          }, seconds* 1000);
+          return "timer set for " + seconds + " seconds";
           
        }
        else {
@@ -69,7 +87,7 @@ function getReply(command) {
 }
 
 
-console.log(getReply("set a timer for 5 min"));
+console.log(getReply("set a timer for 4 seconds"));
+// console.log(getReply("Remove clean the car from my todo"));
 
-
- 
+ console.log(toDoList);
