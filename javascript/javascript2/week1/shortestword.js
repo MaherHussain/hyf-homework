c750e0ba46fdc60ac2e6510626873d394b93af96
Maher <maherhussain6@gmail.com>
2020-06-17 20:13:41 +0000

    shortestWord is modefied

--- javascript/javascript2/week1/shortestword.js
+++ javascript/javascript2/week1/shortestword.js
@@ -1,8 +1,30 @@
 const danishWords = ["plante" ,"bil" , "kaffe", "bog", "øl", "planetarium","p"];
-function shortestWord(){
-   console.log(
-  danishWords.reduce((a,b) => a.length <= b.length? a:b));
+function shortestWord(str){
+  
+  if(Array.isArray(str)){
+    let words = str;
+    console.log(words);
+     let shortest = words.reduce((shortestWord, currentword) => {
+       return currentword.length < shortestWord.length
+         ? currentword
+         : shortestWord;
+     }, words[0]);
+     return shortest;
+  }
+  else{
+    let words = str.split(" ");
+    console.log(words);
+    let shortest = words.reduce((shortestWord, currentword) => {
+      return currentword.length < shortestWord.length
+        ? currentword
+        : shortestWord;
+    }, words[0]);
+return shortest;
+  }
+  
+
+  
     
 }
 
-shortestWord();
+console.log(shortestWord("the most important is"));
