33d84de216ba55820bbc86c04b81fd40f17571a7
Maher <maherhussain6@gmail.com>
2020-06-17 23:57:36 +0000

    danishLetters is modefied

--- javascript/javascript2/week1/danishLetters.js
+++ javascript/javascript2/week1/danishLetters.js
@@ -1,47 +1,37 @@
 
+
 function danishLetters(str) {
+ let smallLetters = str.toLowerCase();
   let letter_count1 = 0; // this "å" counter
   let letter_count2 = 0; // this "ø" counter
   let letter_count3 = 0; // this "æ" counter
   
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].toLowerCase() == "å") {
+  for (let i = 0; i < smallLetters.length; i++) {
+    if (smallLetters[i] == "å" ) {
       letter_count1 += 1;
-    } else if (str[i].toLowerCase() == "ø") {
-      letter_count2 += 1;
-    } else if (str[i].toLowerCase() == "æ") {
-             letter_count3 += 1;
-           } 
+    } else if (smallLetters[i] == "ø") {
+             letter_count2 += 1;
+           } else if (smallLetters[i] == "æ") {
+                    letter_count3 += 1;
+                  }
   }
   // check if there is danish letters 
   if (
-    str.toLowerCase().indexOf("å") === -1 &&
-    str.toLowerCase().indexOf("ø") === -1 &&
-    str.toLowerCase().indexOf("æ") === -1
+    smallLetters.indexOf("å") == -1 &&
+    smallLetters.indexOf("ø") == -1 &&
+    smallLetters.indexOf("æ") == -1
   ) {
-    return "there is no any danish letters in your sentenece" ;
-  }
-  
-  else{ 
-      
-         
-          return  "å = " + letter_count1 + 
-          " ø =" + letter_count2 + 
-          " æ = " + letter_count3 +
-          " total = " + (letter_count1 + letter_count2 + letter_count3)
-      
-    }
+    return "there is no any danish letters in your sentenece";
+  } else {
+    let result = {};
+    result.ø = letter_count2;
+    result.å = letter_count1;
+    result.æ = letter_count3;
 
-  
-   
-     
-    
-     
-   
-      
- 
+    return result;
+  }
   
 }
      
 
-console.log(danishLetters("Jeg har en bl bil æåøøøø"))
+console.log(danishLetters("Jeg har en bl bil ÅÅÅ "))
