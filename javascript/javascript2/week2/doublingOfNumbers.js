70c0e438b73839402ba1e7d738ec26cbdd34a0e1
Maher <maherhussain6@gmail.com>
2020-06-18 09:56:14 +0000

    doubling of number exercis added

---
+++ javascript/javascript2/week2/doublingOfNumbers.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3, 4];
+ let newNumbers =[]
+
+   const filtering=numbers.filter(
+     item=>item % 2 !== 0
+ 
+)
+newNumbers = filtering.map((newitem) => newitem * 2);
+console.log("The doubled numbers are", newNumbers);
+//console.log(newNumbers); // [2, 6]
