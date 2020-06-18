d267e2f01c7fbfade54427e04d08af0134988599
Maher <maherhussain6@gmail.com>
2020-06-18 10:20:53 +0000

    doublin of numbers exercis is modefied

--- javascript/javascript2/week2/doublingOfNumbers.js
+++ javascript/javascript2/week2/doublingOfNumbers.js
@@ -1,10 +1,9 @@
 let numbers = [1, 2, 3, 4];
  let newNumbers =[]
 
-   const filtering=numbers.filter(
-     item=>item % 2 !== 0
- 
-)
-newNumbers = filtering.map((newitem) => newitem * 2);
+    newNumbers = numbers
+      .filter((item) => item % 2 !== 0)
+      .map((item) => item * 2);
+
 console.log("The doubled numbers are", newNumbers);
 //console.log(newNumbers); // [2, 6]
