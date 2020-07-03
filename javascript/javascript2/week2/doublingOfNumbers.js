a8d39ce9922a265e8c4b3b07f376f60fa6925755
Maher Hussain <48909747+MaherHussain@users.noreply.github.com>
2020-07-03 07:31:34 +0100

    Delete doublingOfNumbers.js

--- javascript/javascript2/week2/doublingOfNumbers.js
+++
@@ -1,9 +0,0 @@
-let numbers = [1, 2, 3, 4];
- let newNumbers =[]
-
-    newNumbers = numbers
-      .filter((item) => item % 2 !== 0)
-      .map((item) => item * 2);
-
-console.log("The doubled numbers are", newNumbers);
-//console.log(newNumbers); // [2, 6]
