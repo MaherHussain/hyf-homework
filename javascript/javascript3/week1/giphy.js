2532ea914e0c01c9f724526d6af755283a655733
Maher <maherhussain6@gmail.com>
2020-07-03 17:18:27 +0000

    giphy modefied

--- javascript/javascript3/week1/giphy.js
+++ javascript/javascript3/week1/giphy.js
@@ -13,29 +13,31 @@ let imgWrapper = document.querySelector("#imgWrapper")
 const search = sumbitBtn.onclick=(e)=>{
  e.preventDefault();
 
- if (searchWord.value == null || searchWord.value.trim() == "") {
+ if (searchWord.value === "" || searchWord.value == null ) {
    empty.innerHTML = "is empty";
  } else {
-   
-   let newapiUrl = apiUrl + searchWord.value + "&limit=" + gifNumber.value;
-   console.log(newapiUrl);
-   
-   fetch(newapiUrl)
-   
-     .then((respons) => respons.json())
-    
-     .then((result) => {
-       for (let i = 0; i < result.data.length; i++) {
-         let image = document.createElement("img");
-         image.src = result.data[i].images.downsized_medium.url;
-         imgWrapper.insertAdjacentElement("afterbegin", image);
-       }
-     
-       console.log(result);
-     })
-     .catch((err) => {
-       console.error(err);
-     });
+   if (gifNumber.value === ""){
+empty.innerHTML = "";
+let newapiUrl = apiUrl + searchWord.value + "&limit=" + gifNumber.value;
+console.log(newapiUrl);
+
+fetch(newapiUrl)
+  .then((respons) => respons.json())
+
+  .then((result) => {
+    for (let item of result.data) {
+      
+      let image = document.createElement("img");
+      image.src = item.images.downsized_medium.url;
+      imgWrapper.insertAdjacentElement("afterbegin", image);
+    }
+
+    console.log(result);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+   } 
  }
   imgWrapper.innerHTML = "";
 
