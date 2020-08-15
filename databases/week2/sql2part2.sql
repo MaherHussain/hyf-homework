92f15dc25616fc48c5d625dc9cf678d227e4e088
Maher <maherhussain6@gmail.com>
2020-08-15 10:15:43 +0000

    changes are added

--- databases/week2/sql2part2.sql
+++ databases/week2/sql2part2.sql
@@ -1,18 +1,19 @@
 CREATE DATABASE schooldb;
-CREATE TABLE `Class` (
+CREATE TABLE `class` (
 `id` int(10) unsigned not null auto_increment primary key,
 `name` varchar(255) not null,
 `begins` datetime not null,
-`ends` datetime not null 
+`ends` datetime not null
+
 );
 CREATE TABLE `student` (
 `id` int(10) unsigned not null auto_increment primary key,
 `name` varchar(255) not null,
 `email` varchar(255) not null,
-`phone` varchar(255) not null ,
-`class_id` int unsigned not null  ,
- FOREIGN KEY (class_id) REFERENCES class (id)
+`phone` varchar(255) not null 
+
 );
+
 
 create index name_index
 on student (name);
@@ -20,3 +21,4 @@ on student (name);
 ALTER TABLE  Class
 add  status enum('not-started', 'ongoing', 'finished') not null;
 select * from class
+
