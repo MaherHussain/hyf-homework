f953b0af249ba416d362606fc88918a5a8477698
Maher <maherhussain6@gmail.com>
2020-09-02 18:46:39 +0000

    add mealsharing file to homework

---
+++ nodejs/week2/mealSharing/src/backend/app.js
@@ -0,0 +1,51 @@
+
+
+const express = require("express");
+const app = express();
+
+
+
+const meals = require("./routes/meals.js");
+
+const cheapmeal = require("./routes/cheapMeals");
+
+const largemeals = require("./routes/largemeals");
+const randomMeal = require("./routes/meal");
+
+const reservations = require("./routes/reservations");
+const randomreservations = require("./routes/randomReservation");
+app.get("/", async (request, response) => {
+  response.send("Meal Sharing");
+});
+
+app.get("/meals", (request, response) => {
+  response.send(meals);
+});
+
+app.get("/cheapMeals", (request, response) => {
+  response.send(cheapmeal);
+});
+app.get("/largeMeals", (request, response) => {
+  response.send(largemeals);
+});
+app.get("/meal", (request, response) => {
+  response.send(randomMeal);
+});
+app.get("/reservations", (request, response) => {
+  response.send(reservations);
+});
+
+app.get("/randomreservation", (request, response) => {
+  response.send(randomreservations);
+});
+module.exports = app;
+
+
+// import data from meals here
+
+
+
+
+
+
+
