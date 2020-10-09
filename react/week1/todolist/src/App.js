2edcda0967a8cb82681b92536df991042896c17f
Maher <maherhussain6@gmail.com>
2020-10-09 12:14:09 +0000

    implement changes App.js

--- react/week1/todolist/src/App.js
+++ react/week1/todolist/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 const toDoList = [
@@ -58,7 +58,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <h1>
          todo list ...................
         </h1>
